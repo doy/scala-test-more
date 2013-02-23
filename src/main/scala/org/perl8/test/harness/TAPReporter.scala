fa9b8522a5ea474b63d1dad5108ee7b1c858c9ed
Jesse Luehrs <doy@tozt.net>
2013-02-23 01:24:12 -0600

    make this pass the outputstream for now
    will clean this up later

--- src/main/scala/org/perl8/test/harness/TAPReporter.scala
+++ src/main/scala/org/perl8/test/harness/TAPReporter.scala
@@ -1,9 +1,11 @@
 package org.perl8.test.harness
 
+import java.io.OutputStream
+
 import org.perl8.test.Test
 import Utils._
 
 class TAPReporter extends Reporter {
   def run (testName: String): Int =
-    newInstance[Test](testName).run
+    newInstance[Test, OutputStream](testName, System.out).run
 }
