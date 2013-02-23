18854928e8697f733d2758d274151999bcc4a238
Jesse Luehrs <doy@tozt.net>
2013-02-23 15:18:44 -0600

    start working on docs

--- src/main/scala/org/perl8/test/Test.scala
+++ src/main/scala/org/perl8/test/Test.scala
@@ -1,5 +1,10 @@
 package org.perl8.test
 
+/** Base trait for test classes in this framework
+  */
 trait Test {
+  /** Runs the test. The TAP stream will be written to Console.out and
+    * Console.err, so you can swap these out as required in order to parse it.
+    */
   def run (): Int
 }
