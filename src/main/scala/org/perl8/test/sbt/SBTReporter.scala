650cfdf8fd45287f956c46cd3f00dab6cb896ad3
Jesse Luehrs <doy@tozt.net>
2013-02-28 17:44:14 -0600

    use SummarizedTests in SBTReporter

--- src/main/scala/org/perl8/test/sbt/SBTReporter.scala
+++ src/main/scala/org/perl8/test/sbt/SBTReporter.scala
@@ -3,24 +3,17 @@ package org.perl8.test.sbt
 import java.io.ByteArrayOutputStream
 import org.scalatools.testing
 
-import org.perl8.test.harness.Reporter
-import org.perl8.test.tap.Parser
+import org.perl8.test.harness.{Reporter,SummarizedTests}
 import org.perl8.test.Test
 
 class SBTReporter (
   loader:       ClassLoader,
   loggers:      Array[testing.Logger],
   eventHandler: testing.EventHandler
-) extends Reporter {
+) extends Reporter with SummarizedTests {
   def run (testName: String): Int = {
     val test = loader.loadClass(testName).newInstance.asInstanceOf[Test]
-
-    val out = new ByteArrayOutputStream
-    Console.withOut(out) {
-      test.runInHarness
-    }
-
-    val result = (new Parser).parse(out)
+    val result = runOneTest(test, e => ())
 
     result.results.foreach { r =>
       val event = new testing.Event {
